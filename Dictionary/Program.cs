853c957aea6f1ce0e29d430bb561dc2d9cbf1a3b
elephantebae <paul.bae.j@gmail.com>
2021-08-17 19:31:05 -0700

    new update

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dictionary
 {
@@ -6,7 +8,25 @@ namespace Dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> newDict = new Dictionary<int, string>();
+
+
+            newDict.Add(1, "Zebra");
+            newDict.Add(2, "Tiger");
+            newDict.Add(3, "Elephant");
+            newDict.Add(4, "Pig");
+            newDict.Add(5, "Shark");
+
+            Console.WriteLine("There are " + newDict.Keys.Count + " Animals in the dictionary");
+            foreach(var i in newDict)
+            {
+                Console.WriteLine(i.Value);
+            }
+
+            Console.WriteLine("The animal in the 3rd pen is a " + newDict[3] + ".");
+            int findKey = newDict.FirstOrDefault(x => x.Value == "Shark").Key;
+            Console.WriteLine("The Shark is in the " + findKey + "th pen.");
+
         }
     }
 }
